ff0e4bff2ffc08ff9f21078f8be0c5cb67bf0377
tt <tt@Mac.lan>
2026-04-21 06:57:11 -0400

    main

--- js/visuals/change-background.js
+++ js/visuals/change-background.js
@@ -1,46 +1,3 @@
-// export function initBgSlider() {
-//     const container = document.querySelector('.bg-slider');
-//     if (!container) return;
-
-//     const slides = container.querySelectorAll('.bg-slide');
-
-//     const images = [
-//         "imgs/rustic rockymoutains-1024.JPEG",
-//         "pages/medical-spa-services/media/peptide.jpeg",
-//         "pages/medical-spa-services/media/AS-Botox2000.webp.png",
-//         "pages/medical-spa-services/media/iv-infusion.jpeg",
-//         "imgs/MadMediSpa-sihlouette.png"
-//     ];
-
-//     // set images
-//     slides.forEach((slide, i) => {
-//         slide.style.backgroundImage = `url(${images[i]})`;
-//     });
-
-//     let current = 0;
-
-//     setInterval(() => {
-//         const currentSlide = slides[current];
-//         const nextIndex = (current + 1) % slides.length;
-//         const nextSlide = slides[nextIndex];
-
-//         // move current out
-//         currentSlide.classList.remove('active');
-//         currentSlide.classList.add('exit-left');
-
-//         // bring next in
-//         nextSlide.classList.add('active');
-//         nextSlide.classList.remove('exit-left');
-
-//         // reset old slide after animation
-//         setTimeout(() => {
-//             currentSlide.classList.remove('exit-left');
-//         }, 1000);
-
-//         current = nextIndex;
-
-//     }, 1000);
-// }
 
 export function initBgSlider() {
     const container = document.querySelector('.bg-slider');
@@ -49,7 +6,7 @@ export function initBgSlider() {
     const slides = container.querySelectorAll('.bg-slide');
 
     const images = [
-        "imgs/rustic rockymoutains-1024.JPEG",
+        // "imgs/rustic rockymoutains-1024.JPEG",
         "pages/medical-spa-services/media/peptide.jpeg",
         "pages/medical-spa-services/media/AS-Botox2000.webp.png",
         "pages/medical-spa-services/media/iv-infusion.jpeg",
