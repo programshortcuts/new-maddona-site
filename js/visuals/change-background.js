623637260ba62979b53f0f17aad40945f19cd028
tt <tt@Mac.lan>
2026-04-21 07:13:47 -0400

    main

--- js/visuals/change-background.js
+++ js/visuals/change-background.js
@@ -6,7 +6,7 @@ export function initBgSlider() {
     const slides = container.querySelectorAll('.bg-slide');
 
     const images = [
-        // "imgs/rustic rockymoutains-1024.JPEG",
+        "imgs/rustic rockymoutains-1024.JPEG",
         "pages/medical-spa-services/media/peptide.jpeg",
         "pages/medical-spa-services/media/AS-Botox2000.webp.png",
         "pages/medical-spa-services/media/iv-infusion.jpeg",
